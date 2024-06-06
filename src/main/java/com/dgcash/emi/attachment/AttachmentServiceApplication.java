8e24173a4dc17e873f0850bc3856576cc89a3e1c
ammar hofo <a.abualhouf@dg-cash.com>
2024-06-06 11:18:41 +0300

    add swagger

--- src/main/java/com/dgcash/emi/attachment/AttachmentServiceApplication.java
+++ src/main/java/com/dgcash/emi/attachment/AttachmentServiceApplication.java
@@ -1,5 +1,7 @@
 package com.dgcash.emi.attachment;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "Attachment API", version = "1.0", description = "Documentation Attachment API v1.0"))
 public class AttachmentServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/dgcash/emi/attachment/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.dgcash.emi.attachment.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@Profile("!prod")
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Value("${application.openapi.dev-url}")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI authOpenApi() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+        return new OpenAPI().servers(List.of(devServer));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
