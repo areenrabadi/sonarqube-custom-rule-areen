4a20e3cebf98eb9171aad3979d17c0c5879f72c7
AreenAlRabadi <a.rabadi@dg-cash.com>
2025-03-12 05:30:18 +0300

    dddff

---
+++ src/main/java/com/test/emi/attachment/controller/Rest.java
@@ -0,0 +1,1 @@
+package com.test.emi.attachment.controller;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RestController;@RestControllerpublic class Rest {    @GetMapping("/hello")    public String helloWorld() {        return "Hello World";    }}
