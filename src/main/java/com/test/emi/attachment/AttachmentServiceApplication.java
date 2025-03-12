4571a773fdc645962bd30d71dc5e6f2575821a11
AreenAlRabadi <a.rabadi@dg-cash.com>
2025-03-12 05:25:50 +0300

    dddff

--- src/main/java/com/test/emi/attachment/AttachmentServiceApplication.java
+++ src/main/java/com/test/emi/attachment/AttachmentServiceApplication.java
@@ -1,12 +1,8 @@
 package com.test.emi.attachment;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication(scanBasePackages = {"com.test"})
 public class AttachmentServiceApplication {
